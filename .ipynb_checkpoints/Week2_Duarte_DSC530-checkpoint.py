37bb52ed794915c1fe810f5ac4a25e5a526e5255
joseduarte <joduarte@my365.bellevue.edu>
2024-12-04 20:04:46 -0600

    Week 2 assignment

---
+++ .ipynb_checkpoints/Week2_Duarte_DSC530-checkpoint.py
@@ -0,0 +1,29 @@
+#Display the text “Hello World! My name is ...."
+print("Hello world, my name is Jose Duarte")
+#Add two numbers together
+num1 = 10
+num2 = 20
+sum = num1 + num2
+print("Sum of num1 & num2 is: ", sum)
+#Subtract a number from another number
+minus = num1 - num2
+print("Minus of num1 & num2 is: ", minus)
+#Multiply two numbers
+multiply = num1 * num2
+print("Multiplication of num1 & num2 is: ", multiply)
+#Divide between two numbers
+divide = num1 / num2
+print("Division of num1 & num2 is: ", divide)
+#Concatenate two strings together (any words)
+string1 = "Hello"
+string2 = "World"
+concatenated = string1 + " " + string2
+print("Concatenate String: ", concatenated)
+#Create a list of 4 items (can be strings, numbers, both)
+list1 = ["Hello", "World", 10, 20.1, True]
+print("Initial list: ", list1)
+#Append an item to your list (again, can be a string, number)
+list1.append("Jose Duarte")
+print("List after append: ", list1)
+#Create a tuple with 4 items (can be strings, numbers, both)
+tuple1 = ("Ausitn", "Texas", 20, 100.1, False)
